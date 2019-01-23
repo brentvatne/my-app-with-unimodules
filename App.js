2e5c5a65dc27a43e6fabeb062437935b9a09b3f6
Ram Ganesan <ramg8x@gmail.com>
2019-01-23 18:02:29 +0530

    Trying expo-font

--- App.js
+++ App.js
@@ -9,6 +9,7 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import { Localization } from 'expo-localization';
+import * as Font from 'expo-font';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -23,10 +24,28 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>{Localization.locale}</Text>
+        <Text style={styles.quote}>Because of your smile, you make life more beautiful.</Text>
       </View>
     );
   }
+
+  async componentWillMount() {
+    try {
+      await this.loadFonts();
+      console.log("font load successful");
+    } catch(e) {
+      console.log("Failed to load the font", e);
+    }
+  }
+
+  async loadFonts() {
+    await Font.loadAsync({
+        //Typography
+        IndieFlower: require("./assets/fonts/IndieFlower.ttf"),
+    })
+  }
 }
+ 
 
 const styles = StyleSheet.create({
   container: {
@@ -34,6 +53,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  quote:{
+    fontSize: 16,
+    //fontFamily: "IndieFlower",
+    margin: 10
   },
   welcome: {
     fontSize: 20,
