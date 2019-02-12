ecd48a460a5520e5528048cd5a7876227b475894
Brent Vatne <brentvatne@gmail.com>
2019-02-12 13:42:48 -0800

    Update for asset and font support

--- App.js
+++ App.js
@@ -1,17 +1,66 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, ScrollView, View } from 'react-native';
+import {
+  Platform,
+  Image,
+  StyleSheet,
+  Text,
+  ScrollView,
+  View,
+} from 'react-native';
 import { Constants } from '@unimodules/react-native-platform';
 import { Localization } from 'expo-localization';
+import { Asset } from 'expo-asset';
+import * as FileSystem from 'expo-file-system';
+import * as Font from 'expo-font';
 
 export default class App extends Component {
+  state = {
+    ready: false,
+  };
+
+  async componentDidMount() {
+    try {
+      // Load font
+      await Font.loadAsync({ barcode: require('./barcode.ttf') });
+      // Cache image locally
+      await Asset.fromModule(require('./image.jpg')).downloadAsync();
+    } catch (e) {
+      alert(e.message);
+    } finally {
+      this.setState({ ready: true });
+    }
+  }
+
   render() {
+    if (!this.state.ready) {
+      return <View />;
+    }
+
     return (
       <ScrollView style={{ flex: 1 }}>
         <View style={styles.container}>
-          <Text style={styles.welcome}>Locale: {Localization.locale}</Text>
-          <Text style={styles.welcome}>
-            Fonts: {JSON.stringify(Constants)}
+          <Text
+            style={[
+              styles.welcome,
+              /* Temporarily use processFontFamily because expo-font scopes fontFamily names */
+              { fontFamily: Font.processFontFamily('barcode') },
+            ]}>
+            Locale: {Localization.locale}
           </Text>
+          { /* This one will load from local cache on phone */ }
+          <Image
+            source={require('./image.jpg')}
+            style={{ width: 200, height: 200 }}
+          />
+          { /* These ones will load over http every time */ }
+          <Image source={require('./icons/icon.png')} />
+          <Image
+            source={{
+              uri:
+                'https://d30j33t1r58ioz.cloudfront.net/static/brand/logo-b-black-512x512.png',
+            }}
+            style={{ width: 200, height: 200 }}
+          />
         </View>
       </ScrollView>
     );
